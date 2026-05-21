56506402b65184e198a2e0f90f720d06f25adbbb
Josemi_M <kotxambre@gmail.com>
2026-05-21 10:25:33 +0200

    Añadido backend DungeonMastersFilms

---
+++ backend/src/main/java/com/example/DungeonMastersFilms/DungeonMastersFilmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.DungeonMastersFilms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonMastersFilmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonMastersFilmsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/DungeonMastersFilms/DungeonMastersFilmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DungeonMastersFilms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonMastersFilmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
